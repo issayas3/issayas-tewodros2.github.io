09642f68c2c81c8dfe07187c908bf433923b6ff8
issayas3 <issayastewodros1@gmail.com>
2022-08-15 20:56:13 +0000

    description in quotes

---
+++ projects/matchy/data.js
@@ -0,0 +1,162 @@
+/**
+ * Part 1
+ *
+ * In this file, we're going to practice
+ * creating and accessing data structues.
+ *
+ * See the README for detailed instructions,
+ * and read every instruction carefully.
+ */
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 1 - Object Creation //////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+var animal = {};
+animal.species = 'Dog';
+animal['name'] = 'Bobby';
+animal.noises = [];
+console.log(animal);
+
+
+/* ## Step 2 - Array Creation
+ 1. [ ] Create a variable named `noises` and assign it to an empty array.
+ 2. [ ] Using **bracket notation** give `noises` it's first element. A string representing a sound your animal might make.
+ 3. [ ] Using an array function add another noise to the end of `noises`.
+ 4. [ ] Go to the array documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array?redirectlocale=en-US
+ 5. [ ] Look through the functions until you find the one that will place an element at the begining of the array.
+ 6. [ ] Add an element to `noises` using this function.
+ 7. [ ] Using **bracket syntax** again, add another element to the end of `noises`. Make sure that the way you do this step would work no matter how many elements `noises` had. In other words, don't hard code the position of the new element.
+ 8. [ ] `console.log` the length of `noises`
+ 9. [ ] `console.log` the last element in `noises` again without hard coding the index.
+ 10. [ ] `console.log` the whole array.
+ 11. [ ] Does it look right?
+ */
+//////////////////////////////////////////////////////////////////////
+// Step 2 - Array Creation ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+var noises = [];
+noises[0] = 'bark'
+noises.splice(noises.length + 1, 0, 'okrrrr');
+noises.push('growl');
+noises.unshift('brrrrrr');
+
+console.log(noises.length, noises[noises.length - 1], noises);
+//////////////////////////////////////////////////////////////////////
+// Step 3 - Combining Step 1 and 2 ///////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+/* ## Step 3 - Combining Step 1 and 2
+ 1. [ ] Using **bracket syntax**, assign the `noises` property on `animal` to our new `noises` array.
+ 2. [ ] Using any syntax add another noise to the `noises` property on `animal`.
+ 3. [ ] `console.log` `animal`.
+ 4. [ ] Does it look right?
+*/
+
+
+
+animal['noises'] = noises;
+animal.noises.push('sheesh');
+console.log(animal);
+
+
+/* *******************************************************************
+ * Step 4 - Review
+ *
+ * 1. What are the different ways you can access properties on objects?
+ * With "Brack" or "Dot" notation
+ * 2. What are the different ways of accessing elements on arrays?
+ * With 
+ * *******************************************************************
+ */
+
+/* *******************************************************************
+ * Step 5 - Take a Break!
+ *
+ * It's super important to give your brain and yourself
+ * a rest when you can! Grab a drink and have a think!
+ * For like 10 minutes, then, BACK TO WORK! :)
+ * *******************************************************************
+ */
+
+//////////////////////////////////////////////////////////////////////
+// Step 6 - A Collection of Animals //////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+/*
+## Step 6 - A Collection of Animals
+ 1. [ ] Create a variable named `animals` and assign it to an empty array.
+ 2. [ ] `push` our `animal` that we created to `animals`. You should now see your first animal appear on your `index.html` page!
+ 3. [ ] `console.log` `animals`. What does it look like?
+ 4. [ ] Create a variable called `duck` and assign it to the data:
+  - `{ species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh'] }`
+ 5. [ ] `push` `duck` to `animals`
+ 6. [ ] `console.log` `animals`. What does it look like?
+ 7. [ ] Create two more animal objects each with a species, a name, and at least two sounds sounds and add each one of them to `animals.
+ 8. [ ] `console.log` `animals`, and, `console.log` the length of `animals`. Is everything looking right?
+ */
+
+var animals = [];
+animals.push(animal)
+console.log(animals)
+var duck = {species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh']}
+animals.push(duck)
+console.log(animals)
+var panda = {species: 'bear', name: 'Plain Panda', noises: ['much', 'roar']};
+var axelotyl = {species: '?', name: 'qubert', noises: ['squish', 'squash']};
+animals.push(panda, axelotyl)
+console.log(animals, animals.length)
+//////////////////////////////////////////////////////////////////////
+// Step 7 - Making Friends ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+/*
+## Step 7 - Making Friends
+
+Imagine that our website has a profile page for each animal. On this profile page we can see a list of each animal's friend on the website. Just like how people have a list of friends or followers on facebook or instagram. What would be a good data structure to hold this list of friends?
+
+ 1. [ ] Choose a data structure for this **list** of friends.
+ 2. [ ] Write a comment in your code that explains why you chose this data structure.
+ 3. [ ] Create a variable called `friends` and assign it to the data structure that you chose.
+ 4. [ ] Take a look at the documentation for `Math.random` here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ 5. [ ] Write a function called `getRandom` that takes our `animals` array and returns  a random `index` of the input array, using `Math.random`
+ 6. [ ] Using a random index from this function that you just created, get a random animal and add its `name` to `friends`.
+ 7. [ ] `console.log` `friends`.
+ 8. [ ] Using **bracket notation**, add the `friends` list as a **property** also named `friends` on one of the animals in the `animals` array
+ 9. [ ] `console.log` your work.
+*/
+//I choose array as my data structure for a list b/c it is essentially a representation of a list of items.
+var friends = [];
+
+function getRandom(array) {
+ var random = Math.floor(Math.random() * animals.length);
+return random;
+};
+
+friends.push(animals[getRandom()].name)
+animals[1]['friends'] = friends;
+
+console.log(friends)
+
+
+
+
+/**
+ * Nice work! You're done Part 1. Pat yourself on the back and
+ * move onto Part 2 in the file called "functions.js"
+ */
+
+
+
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animal, noises, animals, friends, getRandom;
+if((typeof process !== 'undefined') &&
+   (typeof process.versions.node !== 'undefined')) {
+     module.exports.animal = animal || null;
+     module.exports.noises = noises || null;
+     module.exports.animals = animals || null;
+     module.exports.friends = friends || null;
+     module.exports.getRandom = getRandom || null;
+}
---
+++ projects/matchy/functions.js
@@ -0,0 +1,72 @@
+/**
+ * Part 2
+ *
+ * In this file, we're going to create some
+ * Functions to work with our data created in
+ * data.js.
+ *
+ * See the README for detailed instructions,
+ * and read every instruction carefully.
+ */
+
+//////////////////////////////////////////////////////////////////////
+// Step 1 - Search ///////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+/* 
+## Step 1 - Search
+ 1. Open up the file `functions.js` in your editor.
+ 2. Implement a function declaration called `search` that:
+   - Takes a paramater representing an Array of `animals`.
+   - Takes a paramater representing a String, the name of an animal on which to perform a search.
+   - Looks through the `animals` Array, and returns the animal's Object if an animal with that name exists.
+   - Returns `null` if no animal with that name exists
+ 3. Use the search bar at the top of the page to make sure your function works.
+*/
+
+function search(animalsArray, animalName) {
+    for (var i = 0; i < animalsArray.length - 1; i++) {
+        if (animalsArray[i] === animalName) {
+            return animalsArray[i]
+        } else {
+            return null;
+        }
+    }
+}
+
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 2 - Replace //////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 3 - Remove ///////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 4 - Add ///////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+
+
+/**
+ * You did it! You're all done with Matchy!
+ */
+
+
+
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var search, replace, remove, add;
+if((typeof process !== 'undefined') &&
+   (typeof process.versions.node !== 'undefined')) {
+    module.exports.search = search || null;
+    module.exports.replace = replace || null;
+    module.exports.remove = remove || null;
+    module.exports.add = add || null;
+}
